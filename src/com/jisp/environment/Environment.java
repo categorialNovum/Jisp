fd41ef5260840fbb6156292d4f6f5f65e1bbb8e7
Jon Beaumont <beaumontjs@gmail.com>
2015-03-14 13:12:32 -0400

    environment changes

--- src/com/jisp/environment/Environment.java
+++ src/com/jisp/environment/Environment.java
@@ -1,6 +1,7 @@
 package com.jisp.environment;
 
 import com.jisp.environment.StandardEnvironment;
+import com.jisp.parser.Tokenizer.Symbol;
 import com.jisp.parser.Tokenizer.Token;
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -8,23 +9,39 @@ import java.util.ArrayList;
 public class Environment {
     StandardEnvironment stdEnv;
     private enum Expression{VAR,NUMBER,QUOTE,CONDITIONAL,DEFINITION,PROCEDURE_CALL}
-    HashMap<String,Expression> fullEnv;
+    //HashMap<String,Expression> fullEnv;
+    HashMap<String,String> fullEnv;
 
     public Environment(){
         stdEnv = new StandardEnvironment();
+        fullEnv.put("+", "PLUS");
     }
 
-    public void addToEnv(String s, Expression exp){
-        fullEnv.put(s,exp);
-    }
+//    public void addToEnv(String s, Expression exp){
+ //       fullEnv.put(s,exp);
+  //  }
 
-    public void eval(ArrayList<Token> l){
-        Token t = l.get(0);
-        switch(t.getType()){
-            case SYMBOL:
-            case NUMBER:
-            case PAREN:
+    public Object eval(ArrayList<Object> l){
+        if (l.size() == 0){return null;}
+        Object o = l.get(0);
+        if (o instanceof Symbol){
+            //todo : return mapped value.
+            // figure out best way to map to functions
+        }else if(o instanceof ArrayList){
+            ArrayList currentList = (ArrayList)o;
+            Object currentObj = currentList.get(0);
+
+            //handle quote
+
+            //handle conditional
+
+            //handle define
+
+            //else -> recursive call to eval
+
+
         }
 
+        return o;
     }
 }
